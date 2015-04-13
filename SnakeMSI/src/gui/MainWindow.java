da4bb9022a0bfe2121d8f28155333b84ba72b7b2
Waszker <Waszka23@gmail.com>
2015-04-14 01:12:00 +0200

    Minor and fast additions to MainWindow class.

--- SnakeMSI/src/gui/MainWindow.java
+++ SnakeMSI/src/gui/MainWindow.java
@@ -1,16 +1,51 @@
 package gui;
 
+import java.awt.Label;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  * <p>
  * Main Window class for application. It will become visible after program
- * start.
+ * starts.
  * </p>
  * 
  * @author Piotr Waszkiewicz
  * @version 1.0
- *
+ * 
  */
-public class MainWindow
+public class MainWindow extends JFrame
 {
+	/******************/
+	/* VARIABLES */
+	/******************/
+	private static final long serialVersionUID = 2740437090361841747L;
+	private final String title = "Snake Game";
+
+	/******************/
+	/* METHODS */
+	/******************/
+	public MainWindow()
+	{
+		super();
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(640, 480);
+		this.setLocationRelativeTo(null); // sets window centered
+		this.setTitle(title);
+		this.setResizable(false);
+
+		createLayout();
+	}
+
+	private void createLayout()
+	{
+		this.getContentPane().setLayout(
+				new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+		this.add(new Label("Snake Game"));
+		this.add(new JButton("Start simulation"));
+		
+	}
 
 }
--- SnakeMSI/src/snakemain/Main.java
+++ SnakeMSI/src/snakemain/Main.java
@@ -1,10 +1,14 @@
 package snakemain;
 
+import gui.MainWindow;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
+		MainWindow window = new MainWindow();
+		window.setVisible(true);
 
 	}
 
