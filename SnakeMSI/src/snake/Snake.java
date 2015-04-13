58a490800975976fd7058511fee12bc68fe13894
Filip Turkot <fturkot@gmail.com>
2015-04-13 14:04:06 +0200

    Added enum Movement

--- SnakeMSI/src/snake/Snake.java
+++ SnakeMSI/src/snake/Snake.java
@@ -2,11 +2,11 @@ package snake;
 
 /**
  * <p>
- * Snake class. Provides: decisions, 
+ * Snake class. Provides: decisions and score counting.
  * </p>
  * @author Filip
  */
 public class Snake
 {
-
+	public enum Movement { FORWARD, LEFT, RIGHT };
 }
