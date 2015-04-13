4fe4263427ebc1e43b4fb51e1053d917fe228d49
waszker <waszka23@gmail.com>
2015-04-13 14:03:07 +0200

    Added MainWindow class.

---
+++ SnakeMSI/src/gui/MainWindow.java
@@ -0,0 +1,16 @@
+package gui;
+
+/**
+ * <p>
+ * Main Window class for application. It will become visible after program
+ * start.
+ * </p>
+ * 
+ * @author Piotr Waszkiewicz
+ * @version 1.0
+ *
+ */
+public class MainWindow
+{
+
+}
