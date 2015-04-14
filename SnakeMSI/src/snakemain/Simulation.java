755c5b52c528153c82678f0ff00eebf0af945653
FilipT <turkotf@student.mini.pw.edu.pl>
2015-04-15 01:00:53 +0200

    Added simulation class.

---
+++ SnakeMSI/src/snakemain/Simulation.java
@@ -0,0 +1,20 @@
+package snakemain;
+
+import java.util.Random;
+
+/**
+ * <p>
+ * Class providing snake game logic. It only takes care of
+ * managing snake game map.
+ * Issue who controls the snake (human or CPU) is completely transparent.
+ * </p>
+ * @author Filip
+ */
+public class Simulation
+{
+	/* VARIABLES */
+	private Random r;
+	private byte[][] map;
+	
+	/* METHODS */
+}
