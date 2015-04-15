59fd1c9e78fff012f0b496265594d28ed65c20c2
FilipT <turkotf@student.mini.pw.edu.pl>
2015-04-15 19:04:28 +0200

    removed todo

--- SnakeMSI/src/snakemain/Main.java
+++ SnakeMSI/src/snakemain/Main.java
@@ -6,10 +6,8 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 		MainWindow window = new MainWindow();
 		window.setVisible(true);
-
 	}
 
 }
